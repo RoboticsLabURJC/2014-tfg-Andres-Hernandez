dbdd3248ab7663f3fc7eb7457b41f9cfd762121c
andresjhe <andresjhe@gmail.com>
2018-02-11 23:10:48 +0100

    Gazebo components
    Components created and developed by me for Gazebo during my TFG

---
+++ gazebo/plugins/car/CMakeFiles/carMotors.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/carMotors.dir/carMotors.cc.o"
+  "libcarMotors.pdb"
+  "libcarMotors.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/carMotors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
